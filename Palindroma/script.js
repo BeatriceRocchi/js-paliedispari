cb2a1974e41da2d98d189964a69308b020342161
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-02-28 15:06:02 +0100

    [add] palindrome check in js

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,1 +1,26 @@
+const inputWord = document.getElementById("word");
+const btn = document.querySelector("button");
 
+btn.addEventListener("click", function () {
+  document.querySelector(".output").innerHTML = checkPalindrome(inputWord.value)
+    ? "La parola è palindroma"
+    : "La parola non è palindroma";
+});
+
+//FUNCTION
+function checkPalindrome(wordString) {
+  const wordArray = wordString.split("");
+  console.log(wordArray);
+  console.log(wordArray.length);
+
+  for (let i = 0; i < wordArray.length / 2; i++) {
+    let isPalindrome;
+
+    if (wordArray[i] === wordArray[wordArray.length - 1 - i]) {
+      isPalindrome = true;
+    } else {
+      return (isPalindrome = false);
+    }
+    return isPalindrome;
+  }
+}
