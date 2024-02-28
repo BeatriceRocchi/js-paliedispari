eaadb29ba86ed11c798d85de71aa5ba6493d3b3c
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-02-28 17:53:28 +0100

    [add] style in both exercises and [add] validation on input

--- PariDispari/script.js
+++ PariDispari/script.js
@@ -2,17 +2,31 @@ const btn = document.querySelector("button");
 
 btn.addEventListener("click", function () {
   const numberUser = parseInt(document.getElementById("number-input").value);
-  const evenOddUser = document.getElementById("even-odd-input").value;
-  const numberPC = randomNumber(1, 5);
+  const evenOddUser = document
+    .getElementById("even-odd-input")
+    .value.toLowerCase();
 
-  const sum = numberUser + numberPC;
+  console.log(evenOddUser);
+  console.log(numberUser);
 
-  document.querySelector(".output").innerHTML = `Hai scelto ${evenOddUser} e hai giocato ${numberUser}. Il PC ha giocato ${numberPC}. La somma è pari a ${sum}.`
+  if (evenOddUser !== "pari" && evenOddUser !== "dispari") {
+    alert("Attenzione! Devi inserire 'pari' o 'dispari");
+  } else if (numberUser < 1 || numberUser > 5 || isNaN(numberUser)) {
+    alert("Attenzione! Devi inserire un numero valido");
+  } else {
+    const numberPC = randomNumber(1, 5);
 
-  document.querySelector(".output-result").innerHTML =
-    evenOdd(sum) === evenOddUser
-      ? "Complimenti hai vinto!"
-      : "Peccato, questa volta ha vinto il PC!";
+    const sum = numberUser + numberPC;
+
+    document.querySelector(
+      ".output"
+    ).innerHTML = `Hai scelto ${evenOddUser} e hai giocato ${numberUser}. Il PC ha giocato ${numberPC}. La somma è uguale a ${sum}.`;
+
+    document.querySelector(".output-result").innerHTML =
+      evenOdd(sum) === evenOddUser
+        ? "Complimenti hai vinto!"
+        : "Peccato, questa volta ha vinto il PC!";
+  }
 });
 
 //FUNCTIONS
